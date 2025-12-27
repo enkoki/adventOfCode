2ef52eecbe247dced024374251452523141e0db6
Rijan Dahal <70582929+enkoki@users.noreply.github.com>
2025-12-27 18:37:31 +0545

    Reduced redundancy by using function
    - Binary printing now inside a single function
    - Original output unchanged
    - And testing commit without -m

--- bit_man.c
+++ bit_man.c
@@ -1,18 +1,22 @@
 // Learning Bit Manipulation
 #include <stdint.h>
 #include <stdio.h>
+void printingBinary(int n){
+    for(int i = 7; i >= 0 ;i--){
+        printf("%u", ((n >> i) & 1));
+        if(i % 4 == 0) printf(" ");  //I like 0000 0000 wala representation
+    }
+}
 int main(){
 
     uint8_t x = 10;
     uint8_t y = 0;
     int bit;
+    
     // Decimal to Binary print hanney
     printf("Decimal value = %d", x);
     printf("\nBinary value: ");
-    for(int i = 7; i >= 0 ;i--){
-        printf("%u", ((x >> i) & 1));
-        if(i % 4 == 0) printf(" "); // I like [0000 0000] representation
-    }
+    printingBinary(x);
 
     // Binary lai aaba reverse hanney
     for(int i = 0; i < 8; i++){
@@ -24,10 +28,6 @@ int main(){
     //aaba printing tyo reversed wala binary
     printf("\nDecimal after reversing: %u", y);
     printf("\nBinary value after reversing: ");
-    for(int i = 7; i >= 0 ;i--){
-        printf("%u", ((y >> i) & 1));
-        if(i % 4 == 0) printf(" "); 
-    }
-    printf("\n");
+    printingBinary(y);
     return 0;
 }
