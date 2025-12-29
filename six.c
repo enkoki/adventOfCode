e9cc308db9a6cce569bd67ef6a4d9da1efb70e0b
Rijan Dahal <70582929+enkoki@users.noreply.github.com>
2025-12-29 09:40:56 +0545

    Added a note

--- six.c
+++ six.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 
 int main(){
-    int num[] = {43, 2, 57, 72, 523, 22, 8, 778};
+    int num[] = {43, 2, 57, 72, 523, 22, 8, 778}; // Completed in Python/six.py
     return 0;
 }
